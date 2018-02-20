164d0f51bda23714d436d8687c7e4bb0e8b2f20d
Matt Lawlor <mattlawlor@mattmacbook-pro.student.iastate.edu>
2018-02-19 19:26:43 -0600

    can't get this freakin test to work

---
+++ adventure-zone-website/src/__tests__/header.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Link from '../Link.react';
+import renderer from 'react-test-renderer';
+
+import { Link } from 'react-router-dom';
+import { StaticRouter } from 'react-router';
+import homePage from '../components/pages/homePage';
+import Login from '../components/pages/login';
+import Contact from '../components/pages/contact';
+
+it('Homepage matches', () => {
+    const tree = renderer
+        .create(<Link page="http://localhost:3000/Homepage">Homepage</Link>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('Login matches', () => {
+    const Login = renderer.create(
+        <StaticRouter location="/Login" context={Login}>
+            <Link to="/Login" />
+        </StaticRouter>
+    );
+
+    let tree = Login.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('Contact matches', () => {
+    const Contact = renderer.create(
+        <StaticRouter location="/Contact" context={Contact}>
+            <Link to="/Homepage" />
+        </StaticRouter>
+    );
+
+    let tree = Contact.toJSON();
+    expect(tree).toMatchSnapshot();
+});
