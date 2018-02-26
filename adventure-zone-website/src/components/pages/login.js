f4ec44a8704112fb85bc7d8d73c04db599cc988e
Connor <ckoco1@iastate.edu>
2018-02-26 14:11:57 -0600

    users can opt to login or register

--- adventure-zone-website/src/components/pages/login.js
+++ adventure-zone-website/src/components/pages/login.js
@@ -6,6 +6,8 @@ class Login extends Component {
     constructor() {
         super();
         this.state = {username: '', password: ''};
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleRegister = this.handleRegister.bind(this);
     }
 
     onFieldChange(fieldName) {
@@ -21,14 +23,14 @@ class Login extends Component {
         }
         event.preventDefault();
 
-        const url = 'proj-319.cs.iastate.edu:3000';
-
         let data = {
-            username: event.target.username.value,
-            password: event.target.password.value
+            username: this.state.username.value,
+            password: this.state.password.value
         };
 
-        fetch(url + '/users', {
+        // let url = 'proj-319.cs.iastate.edu/users:3000';
+
+        fetch('proj-319.cs.iastate.edu/users:3000', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -45,14 +47,49 @@ class Login extends Component {
 
             return JSON.stringify(response);
         }).catch(error => {
-            console.error("Error: ", error);
+            // console.error("Error: ", error);
+        });
+    }
+
+    handleRegister(event) {
+        if (!event.target.checkValidity()) {
+            console.log('invalid form');
+            return;
+        }
+        event.preventDefault();
+
+        let data = {
+            username: this.state.username.value,
+            password: this.state.password.value
+        };
+
+        // let url = 'proj-319.cs.iastate.edu/users-new:3000';
+
+        fetch('proj-319.cs.iastate.edu/users-new:3000', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(function (response) {
+            if(!response.ok) {
+                console.error(response.status);
+                throw Error(response.status);
+            }
+
+            alert(response.message);
+
+            return JSON.stringify(response);
+        }).catch(error => {
+            // console.error("Error: ", error);
         });
     }
 
     render() {
         return (
             <div className='Login-page'>
-                <form onSubmit={this.handleSubmit}>
+                <form>
                     <label>
                         Username: <input type="text" value={this.state.username} name="username"
                                          onChange={this.onFieldChange('username').bind(this)}
@@ -63,7 +100,8 @@ class Login extends Component {
                                          onChange={this.onFieldChange('password').bind(this)}
                                          placeholder={'password'}/>
                     </label><br/>
-                    <input type="submit" value="Submit"/>
+                    <button type="submit" onClick={this.handleSubmit}>Submit</button>
+                    <button type="submit" onClick={this.handleRegister}>Register</button>
                 </form>
             </div>
         );
