16d4f7899749202fae55598372b7e9b95d1fa0d7
jhundman <h.hundman@gmail.com>
2018-03-29 21:42:00 -0500

    Got pong working

--- adventure-zone-website/src/index.js
+++ adventure-zone-website/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Pong from './Games/Pong';
+import Pong from './Games/pong';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Pong />, document.getElementById('root'));
